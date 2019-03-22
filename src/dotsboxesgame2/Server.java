628143f5dd45bf604e80156740b4bf55779c1501
ajudev <ajudevnair@gmail.com>
2019-03-22 20:51:56 +0400

    Java code for Server of Dots and Boxes Game.

---
+++ src/dotsboxesgame2/Server.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dotsboxesgame2;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Server {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int port = 8000;
+        System.out.println("Listening at port 8000");
+        ServerSocket serverSocket = new ServerSocket(port);
+        int clientID=1;
+        while (true) {
+            Socket socket = null;
+            try {
+                socket = serverSocket.accept();
+                System.out.println("Connection to server accepted");
+                System.out.println("A new client is connected: "+socket);
+                ObjectOutputStream toClient = new ObjectOutputStream(socket.getOutputStream());
+                ObjectInputStream fromClient = new ObjectInputStream(socket.getInputStream());
+                System.out.println("Assigning client to a thread");
+                MultiThread t = new MultiThread(socket, fromClient, toClient, clientID);
+                t.start();
+                clientID+=1;
+            } catch (FileNotFoundException ex) {
+                socket.close();
+                ex.printStackTrace();
+            }
+        }
+        /*while(true){
+            System.out.println("Listening at port 8000");
+            ServerSocket serversocket = new ServerSocket(8000);
+            Socket socket = serversocket.accept();
+            System.out.println("Connection to server accepted");
+            
+            MultiThread mt = new MultiThread(socket);
+            mt.start();
+            
+            mt.sleep(2000);
+            socket.close();
+        }  */  
+    }
+    
+}
+
